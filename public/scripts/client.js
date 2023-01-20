dfa2daad5378af17676e618c3cbe551595274de1
Pavel <zabolotnikovpg2@mail.ru>
2023-01-20 14:24:49 +0300

    Log Create

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,21 +1,53 @@
-console.log('123123123');
-document.querySelector('#reg_form').addEventListener('submit', async (e) => {
-  e.preventDefault();
-  console.log(e.target);
-  const {
-    name, password, action, method,
-  } = e.target;
+const registration = document.querySelector('#reg_form');
 
-  const res = await fetch(action, {
-    method,
-    headers: {
-      'Content-Type': 'Application/json',
-    },
-    body: JSON.stringify({
-      name: name.value,
-      password: password.value,
-    }),
+const loginization = document.querySelector('#log_form');
+
+const loginError = document.querySelector('.nameLogin');
+
+if (registration) {
+  registration.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const {
+      name, password, action, method,
+    } = e.target;
+
+    const res = await fetch(action, {
+      method,
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+      body: JSON.stringify({
+        name: name.value,
+        password: password.value,
+      }),
+    });
+    const data = await res.json();
+    document.querySelector('message').innerHTML = data.message;
   });
-  const data = await res.json();
-  document.querySelector('message').innerHTML = data.message;
-});
+}
+
+if (loginization) {
+  loginization.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const {
+      name, password, action, method,
+    } = e.target;
+    const res = await fetch(action, {
+      method,
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+      body: JSON.stringify({
+        name: name.value,
+        password: password.value,
+      }),
+    });
+    const data = await res.json();
+    if (data.message === 'ok') {
+      window.location.assign('/');
+    }
+    loginError.innerHTML = data.error;
+  });
+}
--- views/Log.jsx
+++ views/Log.jsx
@@ -5,19 +5,32 @@ module.exports = function Log({ title }) {
   return (
     <Layout title={title}>
       <h2>Login</h2>
-      <link rel="stylesheet" href="/css/log.css" />
-      <div className="divLog">
-        <div className="">Введите ваши данные</div>
-        <div className="">
-          <input className="inputName" placeholder="введите ваше имя" />
-          <input className="inputPass" placeholder="введите пароль" />
-          <a className="buttonLog" href="">
-            {' '}
-            <button className="button4" type="button">Зайти</button>
-          </a>
+      <form action="/auth/log" method="post" id="log_form">
+        <link rel="stylesheet" href="/css/log.css" />
+        <div className="divLog">
+          <div className="">Введите ваши данные</div>
+          <div className="them">
+            <input
+              name="name"
+              type="text"
+              className="inputName"
+              placeholder="введите ваше имя"
+              required
+            />
+            <input
+              name="password"
+              type="password"
+              className="inputPass"
+              placeholder="введите пароль"
+              required
+            />
+            <a className="buttonLog" href=""> </a>
+          </div>
+          <button className="button4" type="submit">Зайти</button>
 
         </div>
-      </div>
+      </form>
+      <span className="nameLogin"></span>
     </Layout>
   );
 };
